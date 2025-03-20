420b3f6c06c6193b2518b651d57880b2dbda5b82
FranciscoPages <pagesfranciscotomas@gmail.com>
2025-03-20 16:46:58 -0300

    Primer cambio después del commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+//Variable: es un espacio de memoria reservado para almacenar informacion importante del programa.
+
+ //Declaracion:
+let cliente;
+ 
+
+ //Asignacion de valores:
+cliente = "Carlos";
+cliente = "Samuel";
+
+console.log(cliente);
+
+
+ //Inicializar variables:
+ let edad = 37;
+
+ console.log(edad);
+ 
+ //Constante: Es una variable cuyo valor no puede ser modificado una vez que ha sido asignado
+
+ const nacimiento = 1987;
+console.log(nacimiento);
+
+//Prueba
+console.log("la consola funciona correctamente")
+ 
+const titulo = document.getElementById("Titulo")
+titulo.textContent = "texto modificado"
